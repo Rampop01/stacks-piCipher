f2b89fa59fe294cb341a4750bacc9611336cbf30
rampop01 <popoolarahmat96@gmail.com>
2026-06-12 20:39:31 +0100

    Implement basic mapping for FAQ question list

--- frontend/src/app/faq/page.js
+++ frontend/src/app/faq/page.js
@@ -10,7 +10,11 @@ export default function FAQ() {
   return (
     <div className="min-h-screen bg-black text-white p-6 md:p-12 font-mono">
       <div className="max-w-4xl mx-auto border-l-2 border-r-2 border-[#FF5500]/30 min-h-[80vh] p-8">
-        FAQ
+        <div className="flex flex-col gap-4">
+          {faqData.map((item, i) => (
+            <div key={i} className="border border-white/10 p-4">{item.q}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
