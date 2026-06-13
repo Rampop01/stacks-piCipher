b97f66eca7103e921c42913f1fd7c15782ba0078
rampop01 <popoolarahmat96@gmail.com>
2026-06-13 19:47:53 +0100

    Create initial Profile page structure

---
+++ frontend/src/app/profile/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Profile() {
+  return (
+    <div className="min-h-screen bg-black text-white p-6 font-mono">
+      <h1>OPERATIVE_PROFILE</h1>
+    </div>
+  );
+}
