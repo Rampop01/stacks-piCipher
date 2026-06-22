ab646c543588e8161400e019b9b9b9ed29464107
rampop01 <popoolarahmat96@gmail.com>
2026-06-22 19:12:24 +0100

    feat(utils): add shallow object merger

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -95,3 +95,4 @@ export const uniqueArray = (arr) => [...new Set(arr)];
 export const cloneArray = (arr) => [...arr];
 export const cloneObject = (obj) => ({...obj});
 export const deepClone = (obj) => JSON.parse(JSON.stringify(obj));
+export const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
