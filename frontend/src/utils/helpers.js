87f0cf5895bfae8b0b68b8e9d47fadc64d848782
rampop01 <popoolarahmat96@gmail.com>
2026-06-22 19:12:23 +0100

    feat(utils): add four decimal precision formatter

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -64,3 +64,4 @@ export const trimSpaces = (s) => s.trim();
 export const isNotEmpty = (val) => val !== null && val !== undefined && val !== '';
 export const isEmpty = (val) => !isNotEmpty(val);
 export const toFixed2 = (num) => Number(num).toFixed(2);
+export const toFixed4 = (num) => Number(num).toFixed(4);
