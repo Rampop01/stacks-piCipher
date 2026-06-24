2eef3360d3a24093ebe65923105a6b9cd21de43b
rampop01 <popoolarahmat96@gmail.com>
2026-06-24 22:52:57 +0100

    feat(utils): implement string whitespace trimmer

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -117,3 +117,4 @@ export const getSearchParams = () => isBrowser() ? new URLSearchParams(window.lo
 export const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 export const lowercase = (s) => s.toLowerCase();
 export const uppercase = (s) => s.toUpperCase();
+export const trimSpaces = (s) => s.trim();
