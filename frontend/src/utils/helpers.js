7933fe77facc558fd0e9976fbfd3a96f8bd13693
rampop01 <popoolarahmat96@gmail.com>
2026-06-26 22:15:17 +0100

    feat(utils): add safe JSON parser wrapper

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -200,3 +200,4 @@ export const isNotEmpty = (val) => val !== null && val !== undefined && val !== 
 export const isEmpty = (val) => !isNotEmpty(val);
 export const toFixed2 = (num) => Number(num).toFixed(2);
 export const toFixed4 = (num) => Number(num).toFixed(4);
+export const parseJSON = (str) => { try { return JSON.parse(str); } catch { return null; } };
