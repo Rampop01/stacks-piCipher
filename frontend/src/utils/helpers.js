9908174b15a24fd8382cdc39e5eedecda99d7264
rampop01 <popoolarahmat96@gmail.com>
2026-06-26 22:15:17 +0100

    feat(utils): add robust array type checker

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -209,3 +209,4 @@ export const getMonth = () => new Date().getMonth() + 1;
 export const getDay = () => new Date().getDate();
 export const addDays = (d, days) => new Date(d.getTime() + days * 86400000);
 export const subDays = (d, days) => new Date(d.getTime() - days * 86400000);
+export const isArray = (arr) => Array.isArray(arr);
